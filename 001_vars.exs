52e0787450091ce08df0933164316580d29f83e6
the-vesuvius <steven.stas92@gmail.com>
2025-02-13 20:12:15 +0200

    vars note update

--- 001_vars.exs
+++ 001_vars.exs
@@ -1,3 +1,11 @@
+# In Elixir terms, assignment is called binding
+
+# In Elixir, a variable name always starts with a lowercase alphabetic character or an
+# underscore. After that, any combination of alphanumeric characters and underscores
+# is allowed. The prevalent convention is to use only lowercase ASCII letters, digits, and
+# underscore
+# Variable names can also end with the question mark (?) or exclamation mark (!)
+# characters
 a = 1
 b = 3
 
